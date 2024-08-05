5de7c27417448f2fb27498de4ba714de75cb7284
nika-jobava481 <jobavanika481@gmail.com>
2024-08-06 01:58:16 +0400

    basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'win11';
+
+  pinned: any[]=[
+    {
+      icon: "../assets/icons/win11.png",
+      id: "win11"
+    },
+    // {
+    //   icon: "../assets/icons/search.svg",
+    //   id: "search"
+    // },
+    {
+      icon: "../assets/icons/fileExplorer.png",
+      id: "file-explorer"
+    },
+    {
+      icon: "../assets/icons/chrome.png",
+      id: "chrome"
+    },
+  ]
 }
